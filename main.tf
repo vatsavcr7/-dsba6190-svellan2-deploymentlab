f55123d2305b7dee5fb0af98ba2a6f0fab02d8e0
vatsav45 <srivatsavreddy45@gmail.com>
2023-04-25 16:41:25 -0400

    storage account

--- main.tf
+++ main.tf
@@ -34,11 +34,16 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  account_tier        = "Standard"
+
   account_replication_type = "LRS"
 
+
+
   tags = local.tags
+
+
 }
